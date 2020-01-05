95ff47f0657a8d30fcbca73a04f9a2f399f8bb6e
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-01-05 17:47:57 +0100

    Badge component

---
+++ src/components/Badge.svelte
@@ -0,0 +1,9 @@
+<script>
+  // Color of the button
+  export let color='blue';
+
+  $: cls = `bg-${color}-500 text-white rounded-full text-xs px-2 py-1 align-top`;
+</script>
+<span class={cls}>
+  <slot></slot>
+</span>
