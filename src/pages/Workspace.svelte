c7494a96d29534bdd6e0e8f635f8bf40fbffa731
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-01-06 00:15:46 +0100

    initial workspace page

--- src/pages/Workspace.svelte
+++ src/pages/Workspace.svelte
@@ -1,5 +1,21 @@
 <script>
+  import { query, getClient } from 'svelte-apollo';
+  import { Qworkspace } from '../queries'
+
   import Card from "../components/Card.svelte";
+
+  // export let id="";
+  export let ownerEmail="";
+  export let name="";
+  const workspace = query(getClient(), {
+    query: Qworkspace,
+    variables: { ownerEmail, name }
+  });
 </script>
+{#await $workspace}
+...
+{:then result}
+<h1>{result.data.workspace.name}</h1>
 <Card>
 </Card>
+{/await}
--- src/queries.js
+++ src/queries.js
@@ -7,6 +7,15 @@ export const Qme = gql`
   }
 }`
 
+export const Qworkspace = gql`
+query Workspace($ownerEmail: String, $name: String) {
+  workspace(ownerEmail: $ownerEmail, name: $name) {
+    id,
+    name
+  }
+}
+`;
+
 export const Qworkspaces = gql`
 {
   workspaces {
