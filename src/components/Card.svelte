5f2a5de4e1edd0510367275b45d0d5c1ab823917
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-01-08 22:27:00 +0100

    allow scrollbars to overflow content

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,3 +1,6 @@
-<div class="bg-white p-3 md:p-4 rounded shadow-md mb-2 md:mb-4">
+<script>
+  export let overflow=false;
+</script>
+<div class="bg-white p-3 md:p-4 rounded shadow-md mb-2 md:mb-4" class:overflow-auto={overflow}>
   <slot></slot>
 </div>
